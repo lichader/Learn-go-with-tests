a5cafbe20c399b30c7b09d6e281481b511a6c55d
lichader <1524470+lichader@users.noreply.github.com>
2024-06-19 22:29:23 +1000

    clockface WIP

---
+++ clockface/clockface.go
@@ -0,0 +1,36 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+const (
+	secondHandLength = 90
+	clockCentreX     = 150
+	clockCentreY     = 150
+)
+
+func SecondHand(tm time.Time) Point {
+	p := secondHandPoint(tm)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}
+	return p
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+	return Point{x, y}
+}

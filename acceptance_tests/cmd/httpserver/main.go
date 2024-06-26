bb8a55c20dab654e71cc1a8ea62c0fb2d0fdebcb
lichader <1524470+lichader@users.noreply.github.com>
2024-06-26 23:08:09 +1000

    Initial cut of acceptance tests

---
+++ acceptance_tests/cmd/httpserver/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	go_specs_greet "github.com/quii/go-specs-greet"
+)
+
+func main() {
+	handler := http.HandlerFunc(go_specs_greet.Handler)
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		log.Fatal(err)
+	}
+}

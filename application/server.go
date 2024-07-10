e0b2eb39c6bab0858f0b7672901130eb2547652f
lichader <1524470+lichader@users.noreply.github.com>
2024-07-10 22:30:32 +1000

    Split get and post, hardcoded http status

--- application/server.go
+++ application/server.go
@@ -7,14 +7,32 @@ import (
 )
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusAccepted)
+		return
+	}
 
+	switch r.Method {
+	case http.MethodGet:
+		p.showScore(w, r)
+	case http.MethodPost:
+		p.processWin(w)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
+
 	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func GetPlayerScore(name string) int {
